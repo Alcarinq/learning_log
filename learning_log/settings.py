ad68f58db2f7d1b130013727e5c4eeb9a3fe70a5
Alcarinq <majstersztyck@gmail.com>
2021-06-25 20:54:25 +0200

    heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xxx'
+SECRET_KEY = 'django-insecure-2s#(z&see+ax0=g+pa&=sld6-l_3-fiu(h9_+w!uprzbugnflu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'users:login'
+
+# heroku
+import django_heroku
+django_heroku.settings(locals())
